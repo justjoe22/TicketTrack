0c52e6842114d9186c485c726665e3d547ae4302
Joe Prewitt <justjoe22@gmail.com>
2016-06-01 12:00:34 -0400

    DataSet rebuild 001

--- TicketTrack/frmTicketSQL.Designer.vb
+++ TicketTrack/frmTicketSQL.Designer.vb
@@ -32,31 +32,15 @@ Partial Class frmTicketSQL
         Me.CopyMenu = New System.Windows.Forms.ContextMenuStrip(Me.components)
         Me.mnuCopy = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuDelete = New System.Windows.Forms.ToolStripMenuItem()
-        'Me.TblTicketBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        'Me.dbTickets_sql_ds = New Global.TicketTrack.dbTickets_sql_ds()
         Me.cmbView = New System.Windows.Forms.ComboBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.time_track = New System.Windows.Forms.Timer(Me.components)
         Me.btnRefresh = New System.Windows.Forms.Button()
-        'Me.ITT00501_PT_TICKT_MSTTableAdapter = New Global.TicketTrack.dbTickets_sql_dsTableAdapters.ITT00501_PT_TICKT_MSTTableAdapter()
-        'Me.TableAdapterManager = New Global.TicketTrack.dbTickets_sql_dsTableAdapters.TableAdapterManager()
         Me.txtRecordCt = New System.Windows.Forms.TextBox()
-        'Me.ITT00501PTTICKTMSTBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.INCDNTIDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.SUMMDESCDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.ASSGNIDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.RLATETXTDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewComboBoxColumn()
-        Me.NOTEDESCDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.FIXFLGDataGridViewCheckBoxColumn = New System.Windows.Forms.DataGridViewCheckBoxColumn()
-        Me.CMPLTFLGDataGridViewCheckBoxColumn = New System.Windows.Forms.DataGridViewCheckBoxColumn()
-        Me.CREATDTMDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.dgSQL, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.CopyMenu.SuspendLayout()
-        'CType(Me.TblTicketBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-        'CType(Me.dbTickets_sql_ds, System.ComponentModel.ISupportInitialize).BeginInit()
-        'CType(Me.ITT00501PTTICKTMSTBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'dgSQL
@@ -65,9 +49,8 @@ Partial Class frmTicketSQL
         DataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black
         Me.dgSQL.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
         Me.dgSQL.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-           Or System.Windows.Forms.AnchorStyles.Left) _
-           Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.dgSQL.AutoGenerateColumns = False
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.dgSQL.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader
         Me.dgSQL.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.dgSQL.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText
@@ -79,9 +62,7 @@ Partial Class frmTicketSQL
         DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
         DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
         Me.dgSQL.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle2
-        Me.dgSQL.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.INCDNTIDDataGridViewTextBoxColumn, Me.SUMMDESCDataGridViewTextBoxColumn, Me.ASSGNIDDataGridViewTextBoxColumn, Me.RLATETXTDataGridViewTextBoxColumn, Me.NOTEDESCDataGridViewTextBoxColumn, Me.FIXFLGDataGridViewCheckBoxColumn, Me.CMPLTFLGDataGridViewCheckBoxColumn, Me.CREATDTMDataGridViewTextBoxColumn})
         Me.dgSQL.ContextMenuStrip = Me.CopyMenu
-        'Me.dgSQL.DataSource = Me.TblTicketBindingSource
         DataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
         DataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window
         DataGridViewCellStyle3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
@@ -109,32 +90,21 @@ Partial Class frmTicketSQL
         Me.CopyMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuCopy, Me.mnuDelete})
         Me.CopyMenu.Name = "CopyMenu"
         Me.CopyMenu.ShowImageMargin = False
-        Me.CopyMenu.Size = New System.Drawing.Size(120, 48)
+        Me.CopyMenu.Size = New System.Drawing.Size(128, 70)
         '
         'mnuCopy
         '
         Me.mnuCopy.Name = "mnuCopy"
         Me.mnuCopy.ShortcutKeyDisplayString = "Ctrl+C"
-        Me.mnuCopy.Size = New System.Drawing.Size(119, 22)
+        Me.mnuCopy.Size = New System.Drawing.Size(127, 22)
         Me.mnuCopy.Text = "&Copy"
         '
         'mnuDelete
         '
         Me.mnuDelete.Name = "mnuDelete"
         Me.mnuDelete.ShortcutKeys = System.Windows.Forms.Keys.Delete
-        Me.mnuDelete.Size = New System.Drawing.Size(119, 22)
+        Me.mnuDelete.Size = New System.Drawing.Size(127, 22)
         Me.mnuDelete.Text = "&Delete"
-        '
-        'TblTicketBindingSource
-        '
-        'Me.TblTicketBindingSource.DataMember = "ITT00501_PT_TICKT_MST"
-        'Me.TblTicketBindingSource.DataSource = Me.dbTickets_sql_ds
-        '
-        'dbTickets_sql_ds
-        '
-        'Me.dbTickets_sql_ds.DataSetName = "dbTickets_sql_ds"
-        'Me.dbTickets_sql_ds.EnforceConstraints = False
-        'Me.dbTickets_sql_ds.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'cmbView
         '
@@ -169,16 +139,6 @@ Partial Class frmTicketSQL
         Me.btnRefresh.Text = "&Refresh"
         Me.btnRefresh.UseVisualStyleBackColor = True
         '
-        'ITT00501_PT_TICKT_MSTTableAdapter
-        '
-        'Me.ITT00501_PT_TICKT_MSTTableAdapter.ClearBeforeFill = True
-        '
-        'TableAdapterManager
-        '
-        'Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
-        'Me.TableAdapterManager.ITT00501_PT_TICKT_MSTTableAdapter = Me.ITT00501_PT_TICKT_MSTTableAdapter
-        'Me.TableAdapterManager.UpdateOrder = Global.TicketTrack.dbTickets_sql_dsTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
-        '
         'txtRecordCt
         '
         Me.txtRecordCt.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
@@ -193,11 +153,6 @@ Partial Class frmTicketSQL
         Me.txtRecordCt.Text = "0"
         Me.txtRecordCt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         Me.txtRecordCt.Visible = False
-        '
-        'ITT00501PTTICKTMSTBindingSource
-        '
-        'Me.ITT00501PTTICKTMSTBindingSource.DataMember = "ITT00501_PT_TICKT_MST"
-        'Me.ITT00501PTTICKTMSTBindingSource.DataSource = Me.dbTickets_sql_ds
         '
         'Label2
         '
@@ -217,72 +172,6 @@ Partial Class frmTicketSQL
         Me.Label3.TabIndex = 6
         Me.Label3.Text = "Complete = Any Ticket that is Resolved or Closed."
         '
-        'INCDNTIDDataGridViewTextBoxColumn
-        '
-        Me.INCDNTIDDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
-        Me.INCDNTIDDataGridViewTextBoxColumn.DataPropertyName = "INCDNT_ID"
-        Me.INCDNTIDDataGridViewTextBoxColumn.HeaderText = "Ticket #"
-        Me.INCDNTIDDataGridViewTextBoxColumn.Name = "INCDNTIDDataGridViewTextBoxColumn"
-        Me.INCDNTIDDataGridViewTextBoxColumn.Width = 72
-        '
-        'SUMMDESCDataGridViewTextBoxColumn
-        '
-        Me.SUMMDESCDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
-        Me.SUMMDESCDataGridViewTextBoxColumn.DataPropertyName = "SUMM_DESC"
-        Me.SUMMDESCDataGridViewTextBoxColumn.HeaderText = "Summary"
-        Me.SUMMDESCDataGridViewTextBoxColumn.Name = "SUMMDESCDataGridViewTextBoxColumn"
-        '
-        'ASSGNIDDataGridViewTextBoxColumn
-        '
-        Me.ASSGNIDDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
-        Me.ASSGNIDDataGridViewTextBoxColumn.DataPropertyName = "ASSGN_ID"
-        Me.ASSGNIDDataGridViewTextBoxColumn.HeaderText = "Assignment"
-        Me.ASSGNIDDataGridViewTextBoxColumn.Name = "ASSGNIDDataGridViewTextBoxColumn"
-        Me.ASSGNIDDataGridViewTextBoxColumn.Width = 86
-        '
-        'RLATETXTDataGridViewTextBoxColumn
-        '
-        Me.RLATETXTDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
-        Me.RLATETXTDataGridViewTextBoxColumn.DataPropertyName = "RLATE_TXT"
-        Me.RLATETXTDataGridViewTextBoxColumn.HeaderText = "Relationship"
-        Me.RLATETXTDataGridViewTextBoxColumn.Items.AddRange(New Object() {"Duplicate of", "Original of", "Related to", "Caused", "Caused by", "Resolved", "Resolved by"})
-        Me.RLATETXTDataGridViewTextBoxColumn.Name = "RLATETXTDataGridViewTextBoxColumn"
-        Me.RLATETXTDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
-        Me.RLATETXTDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic
-        Me.RLATETXTDataGridViewTextBoxColumn.Width = 90
-        '
-        'NOTEDESCDataGridViewTextBoxColumn
-        '
-        Me.NOTEDESCDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
-        Me.NOTEDESCDataGridViewTextBoxColumn.DataPropertyName = "NOTE_DESC"
-        Me.NOTEDESCDataGridViewTextBoxColumn.HeaderText = "Notes"
-        Me.NOTEDESCDataGridViewTextBoxColumn.Name = "NOTEDESCDataGridViewTextBoxColumn"
-        '
-        'FIXFLGDataGridViewCheckBoxColumn
-        '
-        Me.FIXFLGDataGridViewCheckBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader
-        Me.FIXFLGDataGridViewCheckBoxColumn.DataPropertyName = "FIX_FLG"
-        Me.FIXFLGDataGridViewCheckBoxColumn.HeaderText = "Reference"
-        Me.FIXFLGDataGridViewCheckBoxColumn.Name = "FIXFLGDataGridViewCheckBoxColumn"
-        Me.FIXFLGDataGridViewCheckBoxColumn.Width = 63
-        '
-        'CMPLTFLGDataGridViewCheckBoxColumn
-        '
-        Me.CMPLTFLGDataGridViewCheckBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader
-        Me.CMPLTFLGDataGridViewCheckBoxColumn.DataPropertyName = "CMPLT_FLG"
-        Me.CMPLTFLGDataGridViewCheckBoxColumn.HeaderText = "Complete"
-        Me.CMPLTFLGDataGridViewCheckBoxColumn.Name = "CMPLTFLGDataGridViewCheckBoxColumn"
-        Me.CMPLTFLGDataGridViewCheckBoxColumn.Width = 57
-        '
-        'CREATDTMDataGridViewTextBoxColumn
-        '
-        Me.CREATDTMDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
-        Me.CREATDTMDataGridViewTextBoxColumn.DataPropertyName = "CREAT_DTM"
-        Me.CREATDTMDataGridViewTextBoxColumn.HeaderText = "Created Date"
-        Me.CREATDTMDataGridViewTextBoxColumn.Name = "CREATDTMDataGridViewTextBoxColumn"
-        Me.CREATDTMDataGridViewTextBoxColumn.ReadOnly = True
-        Me.CREATDTMDataGridViewTextBoxColumn.Width = 95
-        '
         'frmTicketSQL
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -300,9 +189,6 @@ Partial Class frmTicketSQL
         Me.Text = "TicketTrack"
         CType(Me.dgSQL, System.ComponentModel.ISupportInitialize).EndInit()
         Me.CopyMenu.ResumeLayout(False)
-        'CType(Me.TblTicketBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-        'CType(Me.dbTickets_sql_ds, System.ComponentModel.ISupportInitialize).EndInit()
-        'CType(Me.ITT00501PTTICKTMSTBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
